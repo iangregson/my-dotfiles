671640c9bd657c3d1f4ac6ca4d05b79c5e3f33a9
Ian Gregson <ian@gregson.me>
2017-09-21 18:22:03 +0100

    simplified

--- bin/install.sh
+++
@@ -1,4 +0,0 @@
-#/bin/bash
-cp -f ./zshrc ~/.zshrc
-cp -f ./tmux.conf ~/.tmux.conf
-cp -f ./init.vim ~/.config/nvim/init.vim
--- bin/save.sh
+++
@@ -1,4 +0,0 @@
-#/bin/bash
-cp ~/.zshrc ./zshrc
-cp ~/.tmux.conf ./tmux.conf
-cp ~/.config/nvim/init.vim ./init.vim
